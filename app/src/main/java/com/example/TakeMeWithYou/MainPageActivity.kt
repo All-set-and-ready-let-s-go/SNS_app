ba93d40f91180de142b634169a45bc038ce8c071
june <dydrkfl078@naver.com>
2023-08-16 15:34:53 +0900

    액티비티 이름 변경, 메인페이지 액티비티에 툴바 적용

--- app/src/main/java/com/example/TakeMeWithYou/MainPageActivity.kt
+++ app/src/main/java/com/example/TakeMeWithYou/MainPageActivity.kt
@@ -2,12 +2,11 @@ package com.example.TakeMeWithYou
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.SearchView
 
-class MainActivity : AppCompatActivity() {
+class MainPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_mainpage)
 
 
     }
